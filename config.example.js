99ff2e601da2fc6bf97c817fef61e8023e88aeea
quentin <quentinrichard97@gmail.com>
2020-02-20 12:08:44 +0100

    add config files

---
+++ config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //API
+  API_PORT: 3000,
+  //DATABASE
+  DB_NAME: 'informi_test',
+  DB_USR: 'informi_test',
+  DB_PWD: 'informi_test',
+  DB_HOST: 'localhost',
+  //CONFIG
+  SALT: 'mysalt',
+}
